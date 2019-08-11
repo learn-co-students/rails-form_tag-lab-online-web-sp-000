9efe967640caa574c5df1a3391e6d464bfb7fd06
Ramon Chavez <rachavez1974@gmail.com>
2019-08-11 15:36:14 -0400

    Add new temmplate, create action, and configured route.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show]
 
+  def new
+    
+  end
+  
   def index
     @students = Student.all
   end
 
   def show
+    
+  end
+
+  def create
     
   end
 
