c31a8344ecdc8d4ab980fc9691361d4f8abe9514
jgrantps <jgrantps@hotmail.com>
2019-10-02 16:21:38 -0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    # binding.pry
+    Student.create(:first_name => params[:student][:first_name], :last_name => params[:student][:last_name])
+  redirect_to "show"
+  end
+
+
+
 end
