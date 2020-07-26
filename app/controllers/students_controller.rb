7075b473dc080d9645fef6fc13ad6713b597290d
cantstopcoding <jonathan_koss@yahoo.com>
2020-07-26 14:50:29 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,10 +12,13 @@ class StudentsController < ApplicationController
   end
 
   def create
-    # Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    @student = Student.new 
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
-    redirect_to student_path(@student)
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
