ba480674fe0a8af9c6cdeba77d174e40472785f9
Chad Lengefeld <chadlengefeld@gmail.com>
2020-09-11 06:40:29 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,7 @@ class StudentsController < ApplicationController
   def new
   end
 
+  def create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
   def show
     @student = Student.find(params[:id])
   end
