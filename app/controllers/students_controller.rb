c4965452ae7030173c3f4cf7b04e9fea41a75618
markdrehmann <markdrehmann@gmail.com>
2020-12-18 11:45:39 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,9 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name]) # ?? this isn't going to the new form...
+    redirect_to new_student_path
+  end
+
 end
