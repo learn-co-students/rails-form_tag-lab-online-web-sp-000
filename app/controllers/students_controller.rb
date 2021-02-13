cdcba8aedd8a7f24064b50deb4a666c979df6377
deliaconstantino <dconstan92@gmail.com>
2021-02-13 17:06:07 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,4 +8,29 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    # @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+
+    redirect_back fallback_location: { action: "new", id: @student.id }
+    # render new_student_path
+  end
+
 end
+
+
+# <%= text_field_tag :'student[first_name]', @student.first_name %><br>
+# <%= text_field_tag :'student[last_name]', 'hello' %><br>
+
+# module StudentsHelper
+#   def book_select(f)
+#     options = {}
+#     options['Harry Potter'] = 1 unless @a == 1
+#     options['Lord of the Rings'] = 2 unless @b == 2
+#     f.select :book_id, options
+#   end
+
+# end
