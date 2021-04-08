36df8ca517a96f197f23e3ad2bb57e4d5099f1b7
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-08 12:51:07 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,18 @@
+require 'pry'
 class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+  end
+
+  def new
+
+  end
+
+  def create
+    # binding.pry
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
   end
 
   def show
