8841713511d5ffcf785caab47bacf75392458a8a
rsneal01 <rsneal01@gmail.com>
2021-05-01 11:49:37 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    #  Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # puts params.inspect
+    session[:student] = params
+    # binding.pry
+    redirect_to new_student_path
+  end
+
 end
